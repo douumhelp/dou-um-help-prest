3e3f2df50012442abdeea43cb86667ebebdb7ce2
Matheus <ra23371012-2@aluno.unicesumar.edu.br>
2025-03-08 00:37:51 -0300

    Correção de conflito na branch

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ForgotPassword from "../pages/ForgotPassword";
 import TermsAndConditions from "../pages/Documents/TermsAndConditions";
+import Categories from "../pages/Categories";
 import Home from "../pages/Home";
 
 function RoutesApp(){
@@ -13,6 +14,7 @@ function RoutesApp(){
             <Route path="/register" element={<Register />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/terms" element={<TermsAndConditions />} />
+            <Route path="/categories" element={<Categories />} />
             <Route path="/home" element={<Home />} />
         </Routes>
     )
