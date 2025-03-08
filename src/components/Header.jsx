7f477a4a80763aacc02821df13236a6133f4f047
Matheus <ra23371012-2@aluno.unicesumar.edu.br>
2025-03-08 01:05:51 -0300

    SideBar finalizado, Header iniciado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,68 @@
-import { LayoutGrid, Calendar, User, BarChart, Wallet, MessageCircle } from "lucide-react";
+import { useState } from "react";
+import { Menu, Search, Bell, User } from "lucide-react";
+import { Link } from "react-router";
 
-const navItems = [
-  { icon: <LayoutGrid className="w-5 h-5" />, name: "Dashboard" },
-  { icon: <Calendar className="w-5 h-5" />, name: "Calendário" },
-  { icon: <User className="w-5 h-5" />, name: "Meu Perfil" },
-  { icon: <BarChart className="w-5 h-5" />, name: "Métricas" },
-  { icon: <Wallet className="w-5 h-5" />, name: "Carteira" },
-  { icon: <MessageCircle className="w-5 h-5" />, name: "Chat" },
-];
+const Header = ({ onClick, onToggle }) => {
+  const [isApplicationMenuOpen, setApplicationMenuOpen] = useState(false);
 
-const Header = () => {
+  const toggleApplicationMenu = () => {
+    setApplicationMenuOpen(!isApplicationMenuOpen);
+  };
+
   return (
-    <aside className="fixed top-0 left-0 h-screen w-64 bg-white border-r border-gray-200 p-5">
-      <div className="py-8 flex justify-center">
-        <img
-          src="/logo.png"
-          alt="Logo"
-          width={150}
-          height={40}
-        />
+    <header className="sticky top-0 flex w-full bg-white border-b border-gray-200 z-50">
+      <div className="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
+        <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-gray-200 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
+          <button
+            className="block w-10 h-10 text-gray-500 lg:hidden"
+            onClick={onToggle}
+          >
+            <Menu size={24} />
+          </button>
+
+          <button
+            onClick={onClick}
+            className="hidden w-11 h-11 text-gray-500 border border-gray-200 rounded-lg lg:flex items-center justify-center"
+            >
+            <Menu size={24} />
+        </button>
+
+
+          <Link to="/" className="lg:hidden">
+            <img src="./images/logo/logo.svg" alt="Logo" />
+          </Link>
+
+          <button
+            onClick={toggleApplicationMenu}
+            className="flex items-center justify-center w-10 h-10 text-gray-700 rounded-lg hover:bg-gray-100 lg:hidden"
+          >
+            <Bell size={24} />
+          </button>
+
+          <div className="hidden lg:block">
+            <form action="#" method="POST">
+              <div className="relative">
+                <button className="absolute left-4 top-1/2 -translate-y-1/2">
+                  <Search size={20} className="text-gray-500" />
+                </button>
+                <input
+                  type="text"
+                  placeholder="Buscar Serviço"
+                  className="h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pl-12 pr-14 text-sm text-gray-800 placeholder-gray-400 focus:border-blue-300 focus:ring-3 focus:ring-blue-500/10 xl:w-[430px]"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div
+          className={`${
+            isApplicationMenuOpen ? "flex" : "hidden"
+          } items-center justify-between w-full gap-4 px-5 py-4 lg:flex lg:justify-end lg:px-0`}
+        >
+        </div>
       </div>
-      <nav className="mt-6">
-        <ul className="space-y-2">
-          {navItems.map((item, index) => (
-            <li key={index} className="flex items-center gap-3 p-2 rounded-lg hover:bg-gray-100">
-              {item.icon}
-              <span className="text-gray-900">{item.name}</span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </aside>
+    </header>
   );
 };
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
+import SideBar from "../../components/SideBar";
 import Header from "../../components/Header";
 
 export default function Home() {
     return (
         <div >
+            <SideBar />
             <Header />
         </div>
     );
